eec9d3bf4b06b60aa215ef0fa111ad65a76822e4
Josh Sherman <josh@gravityblvd.com>
2010-04-23 23:00:49 -0400

    Cross-browser bug fixes.
     * Moved all of the code within a conditional so it doesn't try to execute when a system doesn't have support for <canvas>
    
     * Fixed issues related to WebKit browsers (desktop versions of Safari and Chrome)
    
     * Added support for drawing a single dot. Works in FireFox and Safari on the iPad, but seems to be broken on Chrome
    
     * Updated the example page's look and feel.

--- jquery.drawbox.js
+++ jquery.drawbox.js
@@ -1,10 +1,11 @@
 /**
- * jQuery DrawBox Plug-In 0.2
+ * jQuery DrawBox Plug-In 0.3
  *
  * http://github.com/crowdsavings/drawbox
+ * http://plugins.jquery.com/project/drawbox
  *
  * Author: Josh Sherman <josh@crowdsavings.com>
- * Copyright (c) 2010 CrowdSavings.com
+ * Copyright (c) 2010 CrowdSavings.com, LLC
  *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
@@ -26,76 +27,97 @@
 			{
 				if (this.nodeName == 'CANVAS')
 				{
+					$(this).css('cursor', 'pointer');
+
 					if (this.getContext)
 					{
 						var context = this.getContext('2d');
+
+						context.underInteractionEnabled = true;
+
+						// Overrides with passed options
+						if (typeof(options.strokeStyle) != "undefined") { context.strokeStyle = options.strokeStyle; }
+						if (typeof(options.lineWidth)   != "undefined") { context.lineWidth   = options.lineWidth;   }
+						if (typeof(options.lineCap)     != "undefined") { context.lineCap     = options.lineCap;     }
+						if (typeof(options.lineJoin)    != "undefined") { context.lineJoin    = options.lineJoin;    }
+
+						var drawing = false;
+						var offsetX = $(this).attr('offsetLeft');
+						var offsetY = $(this).attr('offsetTop');
+						var x       = false;
+						var y       = false;
+
+						$(this).mousedown(function(e)
+						{
+							drawingStart(e);
+						});
+
+						$(this).mousemove(function(e)
+						{
+							drawingMove(e);
+						});
+
+						$(this).mouseup(function()
+						{
+							drawingStop();
+						});
+							
+						function drawingStart(e)
+						{
+							// iPhone/iPad/iPod support
+							if (typeof(event) != "undefined" && typeof(event.touches) != "undefined")
+							{
+								e = event.touches.item(0);
+							}
+
+							drawing = true;
+
+							x = e.clientX - offsetX;
+							y = e.clientY - offsetY;
+
+							context.moveTo(x, y);
+						}
+							
+						function drawingMove(e)
+						{
+							// Keeps iPad from scrolling while drawing
+							e.preventDefault();
+							
+							// iPhone/iPad/iPod support
+							if (typeof(event) != "undefined" && typeof(event.touches) != "undefined")
+							{
+								e = event.touches.item(0);
+							}
+
+							if (drawing == true)
+							{
+								x = e.clientX - offsetX;
+								y = e.clientY - offsetY;
+
+								context.lineTo(x, y);
+								context.stroke();
+							}
+						}
+
+						function drawingStop()
+						{
+							drawing = false;
+
+							// Draws one last line so we can draw dots (e.g. i)
+							context.lineTo(x, y);
+							context.stroke();
+						}
+
+						// iPad / Touch System events
+						$(this).bind('touchstart',  function(e) { drawingStart(e); });
+						$(this).bind('touchmove',   function(e) { drawingMove(e);  });
+						$(this).bind('touchend',    function(e) { drawingStop(e);  });
+						$(this).bind('touchcancel', function()  { drawingStop();   });
 					}
 					else
 					{
 						alert('Your browser does not support <canvas>');
 					}
-
-					var drawing = false;
-					var offsetX = $(this).attr('offsetLeft');
-					var offsetY = $(this).attr('offsetTop');
-
-					$(this).mousedown(function(e)
-					{
-						drawingStart(e);
-					});
-
-					$(this).mousemove(function(e)
-					{
-						drawingMove(e);
-					});
-
-					$(this).mouseup(function()
-					{
-						drawingStop();
-					});
-						
-					function drawingStart(e)
-					{
-						// iPhone/iPad/iPod support
-						if (typeof(event) != "undefined")
-						{
-							e = event.touches.item(0);
-						}
-
-						drawing = true;
-						context.moveTo(e.clientX - offsetX, e.clientY - offsetY);
-					}
-						
-					function drawingMove(e)
-					{
-						// Keeps iPad from scrolling while drawing
-						e.preventDefault();
-						
-						// iPhone/iPad/iPod support
-						if (typeof(event) != "undefined")
-						{
-							e = event.touches.item(0);
-						}
-
-						if (drawing == true)
-						{
-							context.underInteractionEnabled = true;
-							context.lineTo(e.clientX - offsetX, e.clientY - offsetY);
-							context.strokeStyle = '#000';
-							context.stroke();
-						}
-					}
-
-					function drawingStop()
-					{
-						drawing = false;
-					}
-
-					// iPad / Touch System events
-					$(this).bind('touchstart',  function(e) { drawingStart(e); });
-					$(this).bind('touchmove',   function(e) { drawingMove(e);  });
-					$(this).bind('touchend',    function()  { drawingStop();   });
-					$(this).bind('touchcancel', function()  { drawingStop();   });
 				}
 				else
 				{
