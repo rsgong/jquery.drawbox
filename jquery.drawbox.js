f93a77200d554b73a4fe0265e325981d5f308006
Josh Sherman <josh@gravityblvd.com>
2010-04-25 23:24:46 -0400

    Added tracking of the SVG path and output example.

--- jquery.drawbox.js
+++ jquery.drawbox.js
@@ -1,5 +1,5 @@
 /**
- * jQuery DrawBox Plug-In 0.4
+ * jQuery DrawBox Plug-In 0.5
  *
  * http://github.com/crowdsavings/drawbox
  * http://plugins.jquery.com/project/drawbox
@@ -19,8 +19,17 @@
 		drawbox: function(options)
 		{
 			var defaults = {
-				showClear:  true,
-				clearLabel: 'Clear Canvas'
+				lineWidth:     1.0,
+				lineCap:       'butt',
+				lineJoin:      'miter',
+				miterLimit:    10,
+				strokeStyle:   'black',
+				shadowOffsetX: 0.0,
+				shadowOffsetY: 0.0,
+				shadowBlur:    0.0,
+				shadowColor:   'transparent black',
+				showClear:     true,
+				clearLabel:    'Clear Canvas'
 			}
 
 			var options = $.extend(defaults, options);
@@ -29,30 +38,42 @@
 			{
 				if (this.nodeName == 'CANVAS')
 				{
-					$(this).css('cursor', 'pointer');
+					$(this).css('cursor',   'pointer');
+					$(this).attr('onclick', 'function onclick(event) { void 1; }');
 
 					if (this.getContext)
 					{
 						var canvas  = this;
 						var context = this.getContext('2d');
+						var id      = $(this).attr('id');
 
 						context.underInteractionEnabled = true;
 
 						// Overrides with passed options
-						if (typeof(options.strokeStyle) != 'undefined') { context.strokeStyle = options.strokeStyle; }
-						if (typeof(options.lineWidth)   != 'undefined') { context.lineWidth   = options.lineWidth;   }
-						if (typeof(options.lineCap)     != 'undefined') { context.lineCap     = options.lineCap;     }
-						if (typeof(options.lineJoin)    != 'undefined') { context.lineJoin    = options.lineJoin;    }
+						context.lineWidth     = options.lineWidth;
+						context.lineCap       = options.lineCap;
+						context.lineJoin      = options.lineJoin;
+						context.miterLimit    = options.miterLimit;
+						context.strokeStyle   = options.strokeStyle;
+						context.shadowOffsetX = options.shadowOffsetX;
+						context.shadowOffsetY = options.shadowOffsetY;
+						context.shadowBlur    = options.shadowBlur;
+						context.shadowColor   = options.shadowColor;
 						
 						if (options.showClear == true)
 						{
-							$(this).after('<div class="drawbox-clear">' + options.clearLabel + '</div>');
+							$(this).after('<div id="' + id + '-clear">' + options.clearLabel + '</div>');
 							clear = true;
 						}
+
+						var data_input = id + '-data';
+						$(this).after('<input type="hidden" id="' + data_input + '" name="' + data_input + '" />');
 
 						var drawing = false;
 						var offsetX = $(this).attr('offsetLeft');
 						var offsetY = $(this).attr('offsetTop');
+						var prevX   = false;
+						var prevY   = false;
 						var x       = false;
 						var y       = false;
 
@@ -68,13 +89,15 @@
 						$(this).bind('touchcancel', function()  { drawingStop();   });
 							
 						// Other events
-						$('.drawbox-clear').click(function(e)
+						$('#' + id + '-clear').click(function(e)
 						{
 							context.save();
 							context.beginPath();
 							context.closePath();
 							context.restore();
 							context.clearRect(0, 0, $(canvas).width(), $(canvas).height());
+
+							$('#' + data_input).val('');
 						});
 
 						function getTouch(e)
@@ -84,6 +107,13 @@
 							{
 								e = event.touches.item(0);
 							}
+							
+							// Tracks last position to handle dots (as opposed to lines)
+							if (x != false)
+							{
+								prevX = x;
+								prevY = y;
+							}
 
 							// Calculates the X and Y values
 							x = e.clientX - offsetX;
@@ -92,13 +122,55 @@
 							return e;
 						}
 
+						function draw(type)
+						{
+							var element = $('#' + data_input);
+
+							if (type != 'stop')
+							{
+								if (element.val().length > 0)
+								{
+									element.val(element.val() + ' ');
+								}
+
+								if (type == 'start')
+								{
+									prevX = false;
+									prevY = false;
+
+									var command = 'M';
+									context.moveTo(x, y);
+								}
+								else if (type == 'move')
+								{
+									var command = 'L';
+								
+									// If there's no previous increment since it's a .
+									if (prevX == false)
+									{
+										x = x + 1;
+										y = y + 1;
+									}
+
+									context.lineTo(x, y);
+								}
+
+								context.stroke();
+								element.val(element.val() + command + x + ' ' + y);
+							}
+							else
+							{
+								draw('move');
+							}
+						}
+
 						function drawingStart(e)
 						{
 							drawing = true;
 
 							e = getTouch(e);
 
-							context.moveTo(x, y);
+							draw('start');
 						}
 							
 						function drawingMove(e)
@@ -110,8 +182,7 @@
 							{
 								e = getTouch(e);
 
-								context.lineTo(x, y);
-								context.stroke();
+								draw('move');
 							}
 						}
 
@@ -120,8 +191,7 @@
 							drawing = false;
 
 							// Draws one last line so we can draw dots (e.g. i)
-							context.lineTo(x, y);
-							context.stroke();
+							draw('stop');
 						}
 					}
 					else
